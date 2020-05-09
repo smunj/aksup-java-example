f6f58b12af20fd85e06112836c229474e7ac50f0
Sarthak Munjal <t-samun@microsoft.com>
2020-05-09 17:11:22 +0530

    1

--- src/main/java/helloworld/Hello.java
+++ src/main/java/helloworld/Hello.java
@@ -4,6 +4,6 @@ import static spark.Spark.*;
 
 public class Hello {
     public static void main(String[] args) {
-        get("/", (req, res) -> "Hello World, I'm Java!");
+        get("/", (req, res) -> "Hello World, I'm not Java!");
     }
 }
